89c2a382b55fc1958f92c68852cfd4269c000652
Mikhail Artemev <a.kin192219@gmail.com>
2020-05-07 13:05:33 +0300

    v2

--- webpack.config.js
+++ webpack.config.js
@@ -14,5 +14,13 @@ module.exports = {
     output: {
       filename: '[name].js',
       path: path.resolve(__dirname, 'dist')
+    },
+    
+    module: {
+      rules: [{
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: '/node_modules/'
+      }]
     }
   };
